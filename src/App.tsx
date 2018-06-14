010c23609b9c2cec952500f70d503e3991bae456
Jinto Jose <jintoppy@gmail.com>
2018-06-14 09:09:07 +0530

    making api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import * as superagent from 'superagent';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as React from 'react';
 import './App.css';
@@ -6,19 +7,34 @@ import ProductList from './components/product-list/ProductList';
 import ICartItem from './models/cartItem';
 import IProduct from './models/product';
 
+
 interface IState {
   cart: ICartItem[];
   products: IProduct[];
 };
 
-class App extends React.Component<{}, IState> {
+class App extends React.Component<{}, IState> {  
   public state : Readonly<IState> = {
     cart: [],
-    products: [
-      {"id":1,"title":"Fantastic Granite Chair","description":"Aruba","price":708.00,"imageUrl":"https://unsplash.it/200?image=1"},
-      {"id":2,"title":"Generic Cotton Salad","description":"Steel","price":349.00,"imageUrl":"https://unsplash.it/200?image=2"}
-    ]
+    products: []
   };
+
+  componentWillMount(){
+    superagent
+      .get('http://5b209234ca762000147b254f.mockapi.io/products')
+      .end((err: superagent.ResponseError, res: superagent.Response) => {
+        this.setState({
+          products: res.body
+        });
+      });
+    superagent
+      .get('http://5b209234ca762000147b254f.mockapi.io/cart')
+      .end((err: superagent.ResponseError, res: superagent.Response) => {
+        this.setState({
+          cart: res.body
+        });
+      })
+  }
 
   public render() {
     return (
--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import IProduct from '../../models/product';
+import './Product.css';
 
 interface IProps {
     item: IProduct
@@ -7,8 +8,27 @@ interface IProps {
 class Product extends React.Component<IProps>{
     public render(){
         return (
-            <div className="col-6">  
-                {this.props.item.title}
+            <div className="col-6 product">  
+                <div className="card bg-info">                    
+                    <img className="card-img-top" src={this.props.item.imageUrl} />
+                    <div className="card-body">
+                        <h5 className="card-title">
+                            {this.props.item.title}
+                        </h5>
+                        <p className="card-text">
+                            {this.props.item.description}
+                        </p>
+                        <p className="card-text">
+                            Price: {this.props.item.price}
+                        </p>
+                        <div>
+                            <button className="btn btn-success">
+                                Add to Cart
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                
             </div>
         )
     }
